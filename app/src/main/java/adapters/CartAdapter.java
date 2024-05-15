2c40b996fb884522f6c40e38f25ec84ad70c13bc
Le Minh Hoang <minhhoangle031211@gmail.com>
2024-05-15 23:12:12 +0700

    15_05_2024

--- app/src/main/java/adapters/CartAdapter.java
+++ app/src/main/java/adapters/CartAdapter.java
@@ -6,6 +6,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,89 +14,77 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.clothingstore.R;
 
 import java.util.List;
 
+import models.ClothInfomationModel;
+
 public class CartAdapter extends RecyclerView.Adapter<CartAdapter.MyViewHolder> {
     Context context;
-    List<FoodDetailStore> foodDetailStoreList;
+    List<ClothInfomationModel> clothInfomationModelList;
 
-    public CartAdapter(Context context, List<FoodDetailStore> foodDetailStoreList) {
+    public CartAdapter(Context context, List<ClothInfomationModel> foodDetailStoreList) {
         this.context = context;
-        this.foodDetailStoreList = foodDetailStoreList;
+        this.clothInfomationModelList = foodDetailStoreList;
     }
 
     @NonNull
     @Override
     public CartAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_cart_detail, null);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_cart, null);
         return new MyViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull CartAdapter.MyViewHolder holder, int position) {
-        FoodDetailStore food = foodDetailStoreList.get(position);
-        holder.foodName.setText(food.getName());
-        holder.foodPrice.setText(String.format("$%s", food.getPrice()));
-        holder.foodQuantity.setText(String.valueOf(food.getQuantity()));
-        holder.foodOneTotalPrice.setText(String.format("$%s", food.getPrice() * food.getQuantity()));
-        Glide.with(context).load(food.getImageUrl()).into(holder.foodImage);
+        ClothInfomationModel clothInfomationModel = clothInfomationModelList.get(position);
+        holder.clothName.setText(clothInfomationModel.getName());
+        holder.clothPrice.setText(String.format("$%s", clothInfomationModel.getPrice()));
+        holder.clothSize.setText(String.format("$%s", clothInfomationModel.getSize()));
+        holder.clothQuantity.setText(String.valueOf(clothInfomationModel.getQuantity()));
+        Glide.with(context).load(clothInfomationModel.getImg()).into(holder.clothImage);
     }
 
     @Override
     public int getItemCount() {
-        return foodDetailStoreList.isEmpty()? 0 : foodDetailStoreList.size();
+        return clothInfomationModelList.isEmpty()? 0 : clothInfomationModelList.size();
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
-        ImageView foodImage;
-        TextView foodName, foodPrice, foodQuantity, foodOneTotalPrice;
-        ImageView btnMinus, btnPlus;
+        ImageView clothImage;
+        TextView clothName, clothPrice, clothQuantity, clothSize;
+        Button btnDelete, btnPayment;
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
-            foodImage = itemView.findViewById(R.id.food_cart_image);
-            foodName = itemView.findViewById(R.id.food_cart_detail_name);
-            foodPrice = itemView.findViewById(R.id.food_cart_detail_price);
-            foodQuantity = itemView.findViewById(R.id.food_cart_detail_quantity);
-            foodOneTotalPrice = itemView.findViewById(R.id.food_one_total_price);
-            btnMinus = itemView.findViewById(R.id.btn_cart_minus);
-            btnPlus = itemView.findViewById(R.id.btn_cart_plus);
+            clothImage = itemView.findViewById(R.id.cart_item_image);
+            clothName = itemView.findViewById(R.id.cart_item_name);
+            clothPrice = itemView.findViewById(R.id.cart_item_price);
+            clothSize = itemView.findViewById(R.id.cart_item_size);
+            clothQuantity = itemView.findViewById(R.id.cart_item_quantity);
 
-            btnMinus.setOnClickListener(new View.OnClickListener() {
+            btnDelete.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    int position = getAdapterPosition();
-                    FoodDetailStore food = foodDetailStoreList.get(position);
-                    int currentQuantity = food.getQuantity();
-                    if (food.getQuantity() > 1) {
-                        food.setQuantity(food.getQuantity() - 1);
-                        updateFoodQuantityInDatabase(food);
-                        foodOneTotalPrice.setText(String.format("$%s", food.getPrice() * food.getQuantity()));
-                    }
-                    notifyDataSetChanged();
+
                 }
             });
 
-            btnPlus.setOnClickListener(new View.OnClickListener() {
+            btnPayment.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    int position = getAdapterPosition();
-                    FoodDetailStore food = foodDetailStoreList.get(position);
-                    food.setQuantity(food.getQuantity() + 1);
-                    updateFoodQuantityInDatabase(food);
-                    foodOneTotalPrice.setText(String.format("$%s", food.getPrice() * food.getQuantity()));
-                    notifyDataSetChanged();
+
                 }
             });
 
         }
     }
 
-    private void updateFoodQuantityInDatabase(FoodDetailStore food) {
-        SQLiteDatabase db = new DatabaseHelper(context).getWritableDatabase();
-        ContentValues values = new ContentValues();
-        values.put("quantity", food.getQuantity());
-        db.update("FoodStore", values, "id = ?", new String[]{food.getId()});
-        db.close();
-    }
+//    private void updateClothQuantityInDatabase(ClothInfomationModel cloth) {
+//        SQLiteDatabase db = new DatabaseHelper(context).getWritableDatabase();
+//        ContentValues values = new ContentValues();
+//        values.put("quantity", cloth.getQuantity());
+//        db.update("Clothing Store", values, "_id = ?", new String[]{cloth.getId()});
+//        db.close();
+//    }
 }
