3d3a934b3125a9560727f1b65b9f43d03a2fc33c
Nguyen Thanh Nam <nvthanhnam121@gmail.com>
2024-05-17 02:19:53 +0700

    loading animation for login + register + forgot password

--- app/src/main/java/com/example/clothingstore/ResetPasswordActivity.java
+++ app/src/main/java/com/example/clothingstore/ResetPasswordActivity.java
@@ -11,16 +11,15 @@ import android.widget.Toast;
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.progressindicator.CircularProgressIndicator;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
 import java.util.Objects;
 
 import apis.APIService;
+import interfaces.Callbacks;
 import models.SuccessResponseModel;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -36,8 +35,11 @@ public class ResetPasswordActivity extends AppCompatActivity {
     TextInputLayout layout_confirm_password;
     TextInputEditText input_confirm_password;
     Button change_password_btn;
+    Button btn_login;
+    Button btn_register;
+    CircularProgressIndicator progressIndicator;
     APIService apiService;
-    Validator validator = new Validator();
+    Validator validator;
     String token;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +52,8 @@ public class ResetPasswordActivity extends AppCompatActivity {
     }
 
     private void Mapping() {
+        validator = new Validator();
+        progressIndicator = findViewById(R.id.reset_password_progress);
         layout_otp = findViewById(R.id.reset_password_layout_otp);
         input_otp = findViewById(R.id.reset_password_input_otp);
         layout_password = findViewById(R.id.reset_password_layout_password);
@@ -57,6 +61,8 @@ public class ResetPasswordActivity extends AppCompatActivity {
         layout_confirm_password = findViewById(R.id.reset_password_layout_confirm_password);
         input_confirm_password = findViewById(R.id.reset_password_input_confirm_password);
         change_password_btn = findViewById(R.id.reset_password_btn_change_password);
+        btn_login = findViewById(R.id.reset_password_btn_login);
+        btn_register = findViewById(R.id.reset_password_btn_register);
         input_password.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -103,12 +109,36 @@ public class ResetPasswordActivity extends AppCompatActivity {
         change_password_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ChangePassword();
+                progressIndicator.setVisibility(View.VISIBLE);
+                change_password_btn.setVisibility(View.GONE);
+                ChangePassword(new Callbacks() {
+                    @Override
+                    public void onFunctionCompleted() {
+                        progressIndicator.setVisibility(View.GONE);
+                        change_password_btn.setVisibility(View.VISIBLE);
+                    }
+                });
+            }
+        });
+        Intent loginIntent = new Intent(this, LoginActivity.class);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(loginIntent);
+                finish();
+            }
+        });
+        Intent registerIntent = new Intent(this, RegisterActivity.class);
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(registerIntent);
+                finish();
             }
         });
     }
 
-    private void ChangePassword() {
+    private void ChangePassword(Callbacks resetPasswordCallback) {
         Intent intent = new Intent(this, LoginActivity.class);
         String key = Objects.requireNonNull(input_otp.getText()).toString().trim();
         String password = Objects.requireNonNull(input_password.getText()).toString().trim();
@@ -132,21 +162,23 @@ public class ResetPasswordActivity extends AppCompatActivity {
                 if (response.isSuccessful()) {
                     SuccessResponseModel<String> successResponse = response.body();
                     if (successResponse != null) {
-                        Toast.makeText(ResetPasswordActivity.this, successResponse.getMessage(), Toast.LENGTH_SHORT).show();
                         if (Objects.equals(successResponse.getStatus(), "OK")) {
                             startActivity(intent);
                             finish();
                         }
+                        Toast.makeText(ResetPasswordActivity.this, successResponse.getMessage(), Toast.LENGTH_SHORT).show();
                     }
                 } else {
                     int statusCode = response.code();
                     Toast.makeText(ResetPasswordActivity.this, "Lỗi rồi kìa! Mã lỗi: " + statusCode, Toast.LENGTH_SHORT).show();
                 }
+                resetPasswordCallback.onFunctionCompleted();
             }
 
             @Override
             public void onFailure(@NonNull Call<SuccessResponseModel<String>> call, @NonNull Throwable t) {
-                Toast.makeText(ResetPasswordActivity.this, "Lỗi rồi kìa! (" + t.getMessage() + ")", Toast.LENGTH_SHORT).show();
+                Toast.makeText(ResetPasswordActivity.this, "Đổi mật khẩu không thành công!", Toast.LENGTH_SHORT).show();
+                resetPasswordCallback.onFunctionCompleted();
             }
         });
     }
